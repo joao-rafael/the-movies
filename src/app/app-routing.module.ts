9a7409e03ba0934960aa73a69fb740828466d183
joao-rafael <joaorafa131@gmail.com>
2023-12-05 22:29:22 -0400

    feat: add main router routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './core/pages/details/details.component';
+import { MainComponent } from './core/pages/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'details', component: DetailsComponent },
+  { path: '', component: MainComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
